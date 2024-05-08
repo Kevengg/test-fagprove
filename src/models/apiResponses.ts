5ceb3096d13a904eed30d128337bb45b61a4a4e0
Kevengg <kevinandre.engell@gmail.com>
2024-05-08 10:02:23 +0200

    Update .gitignore and fix typo in apiResponses.ts

--- src/models/apiResponses.ts
+++ src/models/apiResponses.ts
@@ -2,10 +2,10 @@
  * @typedef {object} Response
  * @property {number} status.required - Status code of the response
  * @property {string} message.required - Response message
- * @property {string} data - Data added to responce
+ * @property {string} data - Data added to response
  */
 export default interface Response {
     status: number; // status code
     message?: string; // Response message
-    data?: unknown; // Data added to responce
+    data?: unknown; // Data added to response
 }
